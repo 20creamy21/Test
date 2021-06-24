2afc72d8be55b6b4bb4e2e5e164ab1456945b451
20creamy21 <tsuyoshiwaga4310@gmail.com>
2021-06-24 10:13:28 +0000

    [Add]コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'tests/index'
+  get 'tests/show'
+  get 'tests/new'
+  get 'tests/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
